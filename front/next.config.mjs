e22d6363e017ccb35cbd9051753f35d01cc17a6f
Hectos <hectos@example.com>
2025-05-29 22:31:00 +0200

    Simplify Next.js configuration

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,18 +1,12 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
   images: {
     domains: ['images.unsplash.com', 'randomuser.me'],
     unoptimized: true
   },
-  experimental: {
-    appDir: true,
-    outputFileTracingRoot: path.join(__dirname, '../')
-  }
+  output: 'standalone'
 };
 
 export default nextConfig;
